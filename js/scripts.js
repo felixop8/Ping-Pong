6d638f49b364b54cdaeed6410c30a24f7350eb3f
felixop8 <felixop8@gmail.com>
2017-01-20 12:32:05 -0800

    add fifth spec, it will return ping-pong when the input is divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,9 @@ var pingPong = function(number){
   var arrayNumbers = []
 
   for (var i = 1; i <= number ; i += 1) {
-    if (i % 3 === 0) {
+    if ( i % 15 === 0 ) {
+      arrayNumbers.push("PING--PONG")
+    } else if (i % 3 === 0) {
       arrayNumbers.push("ping"  )
     } else if ( i % 5 === 0) {
       arrayNumbers.push("pong")
