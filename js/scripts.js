a96f1be5c2071401c74acc63eacda8d953d7b7c9
felixop8 <felixop8@gmail.com>
2017-01-20 14:37:37 -0800

    Refactor code, it will looks less redundant and easier to read

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,45 @@
+// BUSINESS LOGIC
+
+var pingPong = function(number) {
+
+  if (number !== number) {
+    return false
+  } else {
+    var arrayNumbers = []
+    for (var i = 1; i <= number ; i += 1) {
+      if ( i % 15 === 0 ) {
+        arrayNumbers.push("PING-PONG")
+      } else if (i % 3 === 0) {
+        arrayNumbers.push("PING")
+      } else if ( i % 5 === 0) {
+        arrayNumbers.push("PONG")
+      } else {
+        arrayNumbers.push(i);
+      };
+    };
+  };
+  return arrayNumbers
+};
+
+
+
 // USER INTERFACE LOGIC
 $(function() {
   $('form#ping-pong').submit(function(event) {
     event.preventDefault();
     var number = parseInt($('input#number').val());
-    var result = pingPong(number);
-    $('#result').text(result)
+    var results = pingPong(number);
+
+    if (!results) {
+      $("#result li").remove();
+      $("#result h4").show();
+      $('#result h4').text("This entry can only contain numbers.");
+    } else {
+      $("#result li").remove();
+      $("#result h4").hide();
+      results.forEach(function(result) {
+        $('#result ul').append("<li>" + result + "</li>");
+      });
+    };
   });
 });
-
-
-
-// BUSINESS LOGIC
-
-var pingPong = function(number){
-
-  if (number !== number) {
-    return false;
-  };
-
-  var arrayNumbers = []
-
-  for (var i = 1; i <= number ; i += 1) {
-    if ( i % 15 === 0 ) {
-      arrayNumbers.push("PING--PONG")
-    } else if (i % 3 === 0) {
-      arrayNumbers.push("ping"  )
-    } else if ( i % 5 === 0) {
-      arrayNumbers.push("pong")
-    } else {
-      arrayNumbers.push(i);
-    }
-  };
-  return arrayNumbers
-}
